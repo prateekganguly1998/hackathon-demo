d81a10f37c2f626886decb3f268dab0318ae1482
Prateek Gangopadhyay <gangopadhyay_p@India.XoriantCorp.com>
2020-12-15 15:53:49 +0530

    multi image

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+app.use(express.static(__dirname + '/public'));
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname,'practice.html'));
+  //__dirname : It will resolve to your project folder.
+});
+router.get('/train',function(req,res){
+  res.sendFile(path.join(__dirname,'learn.html'));
+  //__dirname : It will resolve to your project folder.
+});
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
---
+++ public/train.js
@@ -0,0 +1,90 @@
+let img; let poseNet; let poses = [];
+let trainDirs=['tree','downdog','plank','goddess','warrior2'];
+function setup() {
+    
+    img = createImg('https://static.toiimg.com/photo/msid-69885314/69885314.jpg', imageReady);
+    img.size(640,480);
+    img.elt.crossOrigin="Anonymous";
+    img.style('z-index','-5');
+    img.style('position','absolute')
+    createCanvas(img.width,img.height);
+    img2=createImg('https://static.toiimg.com/photo/msid-69885314/69885314.jpg');
+    img2.size(640,480);
+    // img2.style('margin-left','640px');
+    // img2.style('margin-top','-480px');
+    img2.elt.crossOrigin="Anonymous";
+    img2.style('position','absolute')
+   
+    //img.hide(); // hide the image in the browser
+    frameRate(1); // set the frameRate to 1 since we don't need it to be running quickly in this case
+}
+// when the image is ready, then load up poseNet
+function imageReady(){
+    // set some options
+    let options = {
+        inputResolution: 513,
+        minConfidence: 0.1,
+        architecture:'ResNet50',
+        outputStride:32
+    }
+    
+    // assign poseNet
+    poseNet = ml5.poseNet(modelReady, options);
+
+    // This sets up an event that listens to 'pose' events
+    poseNet.on('pose', function (results) {
+        poses = results;
+        console.log(poses);
+    });
+}
+// when poseNet is ready, do the detection
+function modelReady() {
+    select('#status').html('Model Loaded');
+    
+    // When the model is ready, run the singlePose() function...
+    // If/When a pose is detected, poseNet.on('pose', ...) will be listening for the detection results 
+    // in the draw() loop, if there are any poses, then carry out the draw commands
+    poseNet.singlePose(img)
+}
+// draw() will not show anything until poses are found
+function draw() {
+    if (poses.length > 0) {
+        drawSkeleton(poses);
+        drawKeypoints(poses);
+        noLoop(); // stop looping when the poses are estimated
+    }
+}
+// The following comes from https://ml5js.org/docs/posenet-webcam // A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+    // Loop through all the poses detected
+    for (let i = 0; i < poses.length; i++) {
+        // For each pose detected, loop through all the keypoints
+        let pose = poses[i].pose;
+        for (let j = 0; j < pose.keypoints.length; j++) {
+            // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+            let keypoint = pose.keypoints[j];
+            // Only draw an ellipse is the pose probability is bigger than 0.2
+            if (keypoint.score > 0.2) {
+                fill(255);
+                stroke(20);
+                strokeWeight(4);
+                ellipse(round(keypoint.position.x), round(keypoint.position.y), 8, 8);
+            }
+        }
+    }
+}
+// A function to draw the skeletons
+function drawSkeleton() {
+    // Loop through all the skeletons detected
+    for (let i = 0; i < poses.length; i++) {
+        let skeleton = poses[i].skeleton;
+        // For every skeleton, loop through all body connections
+        for (let j = 0; j < skeleton.length; j++) {
+            let partA = skeleton[j][0];
+            let partB = skeleton[j][1];
+            stroke('red');
+            strokeWeight(1);
+            line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+        }
+    }
+}
