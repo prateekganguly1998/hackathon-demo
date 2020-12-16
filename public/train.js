6b9b499037353991925b39f8863931c047fc5354
Prateek Gangopadhyay <gangopadhyay_p@India.XoriantCorp.com>
2020-12-16 19:00:02 +0530

    16Dec

--- public/train.js
+++ public/train.js
@@ -3,8 +3,72 @@ let video;
 let poseNet;let poseNet2; 
 let poses2=[];
 let pose;
-function setup() {
+color_code = {
+    'nose': [0,0,0],
+    'leftEye': [0,0,0],
+    'rightEye': [0,0,0],
+    'leftEar': [0,0,0],
+    'rightEar': [0,0,0],
+    'leftShoulder': [255,255,0],
+    'rightShoulder': [255,255,0],
+    'leftElbow': [0,255,255],
+    'rightElbow': [0,255,255],
+    'leftWrist': [0,0,255],
+    'rightWrist': [0,0,255],
+    'leftHip': [100,100,0],
+    'rightHip': [100,100,0],
+    'leftKnee': [50,0,0],
+    'rightKnee': [50,0,0],
+    'leftAnkle': [255,0,0],
+    'rightAnkle': [255,0,0]
+    }
+    priority = {
+    'nose': 1,
+    'leftEye': 2,
+    'rightEye': 2,
+    'leftEar': 3,
+    'rightEar': 3,
+    'leftShoulder': 4,
+    'rightShoulder': 4,
+    'leftElbow': 5,
+    'rightElbow': 5,
+    'leftWrist': 6,
+    'rightWrist': 6,
+    'leftHip': 7,
+    'rightHip': 7,
+    'leftKnee': 8,
+    'rightKnee': 8,
+    'leftAnkle': 9,
+    'rightAnkle': 9
+    }
+    var PredictedVal = {}
+    let IdealVal = {
+    'nose': null,
+    'leftEye': null,
+    'rightEye': null,
+    'leftEar': null,
+    'rightEar': null,
+    'leftShoulder': null,
+    'rightShoulder': null,
+    'leftElbow': null,
+    'rightElbow':null ,
+    'leftWrist':null,
+    'rightWrist':null,
+    'leftHip': null,
+    'rightHip': null,
+    'leftKnee': null,
+    'rightKnee': null,
+    'leftAnkle': null,
+    'rightAnkle': null
+    }
+    var ComparisonResult = [];
+    var ValueStore = { 
+        'pred' : {}, 
+        'exp' : {} 
+        } 
+        color_code_keys = Object.keys(color_code); 
     
+function setup() {
     // img = createImg('https://static.toiimg.com/thumb/msid-7647039,width-800,height-600,resizemode-75/7647039.jpg', imageReady);
     // img.size(640,480);
     // img.elt.crossOrigin="Anonymous";
@@ -60,7 +124,16 @@ function image2Ready()
     poseNet2.on('pose', function (results) {
         poses2 = results;
         console.log(poses2);
-        console.log(normalizeVectorCoord(3,4))
+        let currentPose = poses2[0].pose;
+        for (let j = 0; j < Object.keys(currentPose).length-1; j++) { 
+            let keypointj = Object.keys(currentPose)[j+2];
+            if(keypointj)
+            {
+            IdealVal[`${keypointj}`] = currentPose.keypoints[j];
+            
+            }
+        
+            } 
     });
 }
 function model2Ready() {
@@ -70,7 +143,8 @@ function model2Ready() {
     // If/When a pose is detected, poseNet.on('pose', ...) will be listening for the detection results 
     // in the draw() loop, if there are any poses, then carry out the draw commands
     poseNet2.singlePose(img2)
-   
+    
+    // comparePoses() 
 }
 // when the image is ready, then load up poseNet
 function imageReady(){
@@ -98,26 +172,47 @@ function gotPoses(poses)
     }
 }
 function modelLoaded()
-{
+{ select('#status').html('Model Loaded');
     console.log(`Posenet is ready`);
-}
-function normalizeVectorCoord(x,y)
-{
-    let rootSumofSquares=Math.sqrt(x*x+y*y);  
-    let newX=x/rootSumofSquares;
-    let newY=y/rootSumofSquares;
-    return {x:newX,y:newY};
-}
-// when poseNet is ready, do the detection
-function modelReady() {
-    select('#status').html('Model Loaded');
+    //IdealVal is valid here//
+   // console.log(IdealVal);
+    IdealVal=normalizeVectorCoord(IdealVal);
+   // console.log(IdealVal);
+    comparePoses() 
     
-    // When the model is ready, run the singlePose() function...
-    // If/When a pose is detected, poseNet.on('pose', ...) will be listening for the detection results 
-    // in the draw() loop, if there are any poses, then carry out the draw commands
-//    poseNet.singlePose(video)
-   
 }
+function normalizeVectorCoord(Val) 
+{ 
+   
+// /console.log(Val) 
+        let x,i,y; 
+        x= 0; 
+        y = 0; 
+        for(i=0; i< 17;i++){ 
+        // console.log(color_code_keys[i]) 
+        if(Val[color_code_keys[i]])
+        {
+            x += Math.pow(Val[color_code_keys[i]].position.x,2) //*Val[color_code_keys[i]].position.x // console.log('Y',Val[color_code_keys[i]].position.y) 
+            y += Math.pow(Val[color_code_keys[i]].position.y,2) ///Val[color_code_keys[i]].position.y } 
+            let rootSumofSquares=Math.sqrt(x+y); 
+            // console.log(rootSumofSquares,x,y) 
+            // let tx=0,ty=0; 
+            for(i=0; i< 17;i++){ 
+            // console.log(color_code_keys[i]) 
+            Val[color_code_keys[i]].position.x = Val[color_code_keys[i]].position.x/rootSumofSquares;
+            Val[color_code_keys[i]].position.y = Val[color_code_keys[i]].position.y/rootSumofSquares // tx += Val[color_code_keys[i]].position.x*Val[color_code_keys[i]].position.x // ty += Val[color_code_keys[i]].position.y*Val[color_code_keys[i]].position.y // console.log(tx+ ' ' + ty) 
+            // console.log('X',Val[color_code_keys[i]].position.x) 
+            // console.log('Y',Val[color_code_keys[i]].position.y)
+            } 
+        }
+
+        // console.log('TTTTT',tx,ty,tx+ty) // IdealVal = PredictedVal return Val 
+        }
+       // console.log(Val);
+        return Val;
+}   
+// when poseNet is ready, do the detection
+
 // draw() will not show anything until poses are found
 
 function draw()
@@ -130,27 +225,21 @@ function draw()
     image(video,0,0);
     if(pose)
     {
-        // let eyeR=pose.rightEye;
-        // let eyeL=pose.leftEye;
-        // let  d=dist(eyeR.x,eyeR.y,eyeL.x,eyeL.y)
-
-
-
-        // fill(255,0,0);
-        // ellipse(pose.nose.x,pose.nose.y,d);
-        // fill(0,0,255)
-        // ellipse(pose.rightWrist.x,pose.rightWrist.y,32);
-        // ellipse(pose.leftWrist.x,pose.leftWrist.y,32);
+        
 
         for (let i=0;i<pose.keypoints.length;i++)
         {
             let x=pose.keypoints[i].position.x;
             let y=pose.keypoints[i].position.y;
+            PredictedVal[pose.keypoints[i].part] = pose.keypoints[i] ;
+           
             fill(237,191,76);
             ellipse(x,y,12,12)
         }
+       
         for(let i=0;i<skeleton.length;i++)
         {
+
             let cord1=skeleton[i][0];
             let cord2=skeleton[i][1];
             strokeWeight(3);
@@ -169,6 +258,7 @@ function drawKeypoints() {
         for (let j = 0; j < pose.keypoints.length; j++) {
             // A keypoint is an object describing a body part (like rightArm or leftShoulder)
             let keypoint = pose.keypoints[j];
+           
             // Only draw an ellipse is the pose probability is bigger than 0.2
             if (keypoint.score > 0.2) {
                 fill(255);
@@ -194,3 +284,58 @@ function drawSkeleton() {
         }
     }
 }
+
+
+//pose comparison code
+function comparePoses() { 
+   
+    setTimeout(function(){
+        let epsilon = -99; 
+        console.log(Object.keys(PredictedVal));
+        let i; 
+        ComparisonResult = []; 
+        let diffx = 0; 
+        let diffy = 0; 
+        let temp = []; 
+        // [ priority , part , diffx , diffy ] 
+        for(i = 0; i<17 ; i++){ 
+        let temp = []; 
+        temp.push(priority[color_code_keys[i]]); 
+        // temp.push(color_code_keys[i]); 
+        diffx = (IdealVal[color_code_keys[i]].position.x - PredictedVal[color_code_keys[i]].position.x ) 
+        diffy = (IdealVal[color_code_keys[i]].position.y - PredictedVal[color_code_keys[i]].position.y) 
+        // console.log(PredictedVal[color_code_keys[i]],IdealVal[color_code_keys[i]]) if(diffx > epsilon){ 
+        temp.push(diffx) 
+        console.log(temp);
+        } 
+        // else{ 
+        // diffx = 0 
+        // temp.push(diffx) 
+        // } 
+        if(diffy > epsilon){ 
+        temp.push(diffy) 
+        } 
+        else{ 
+        diffy = 0 
+        temp.push(diffy) 
+        }
+        if(diffy != 0 || diffx != 0){ 
+        ComparisonResult.push(temp) 
+        } 
+        
+        // console.log(ComparisonResult) 
+        /* ComparisonResult = [ 
+        [6,657], 
+        [4,677], 
+        [1,57], 
+        [9,67] 
+        ] 
+        console.log(ComparisonResult) 
+        */ 
+        ComparisonResult = ComparisonResult.sort(function(a,b) { 
+        return a[0] - b[0]; 
+        }); 
+        console.log(ComparisonResult) 
+    },2000)
+   
+    }
